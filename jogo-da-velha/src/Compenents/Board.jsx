8e9c404c8236822f5e4df430ea0b1a94420a5146
Vitor Shigueta <vshigueta@gmail.com>
2022-02-01 15:48:27 -0300

    Primeiro commit

---
+++ jogo-da-velha/src/Compenents/Board.jsx
@@ -0,0 +1,72 @@
+import Square from "./Square";
+import React, { useState, useEffect } from 'react';
+
+
+export default function Board(){
+   
+    const [squares , setSquares] = useState( Array(9).fill(null));
+    const [xIsNext ,setNext] = useState(true);
+
+    useEffect(() => {
+        calculateWinner(squares);
+      },[squares]);
+
+    function desenhar(i){
+        return(<Square value={squares[i]} onClick={() => click(i)}  />);
+    }
+
+    function click(i){
+        const squares2 = squares.slice(squares[0]);
+        squares2[i] = xIsNext? "x":"o" ;
+        console.log(i);
+        setSquares(squares2);
+        setNext(!xIsNext); 
+        calculateWinner(squares)
+    }
+
+    function calculateWinner(squares) {
+        const lines = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+          const [a, b, c] = lines[i];
+          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            alert("O ganhador é o: " + squares[a] + "\nClique em OK para iniciar um novo jogo");
+            setSquares(Array(9).fill(null));
+            setNext(true);
+          }
+        }
+        return null;
+    }
+
+    const status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+   
+   return (
+     <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+      {desenhar(0)}
+      {desenhar(1)}
+      {desenhar(2)}
+      </div>
+      <div className="board-row">
+      {desenhar(3)}     
+      {desenhar(4)}
+      {desenhar(5)}
+      </div>
+      <div className="board-row">
+      {desenhar(6)}
+      {desenhar(7)}
+      {desenhar(8)}
+      </div>
+    </div>    
+   )
+}
+
---
+++ jogo-da-velha/src/Game.jsx
@@ -0,0 +1,8 @@
+import Board from "./Compenents/Board";
+
+export default function Game(){
+
+    return(
+        <Board/>
+    );
+}
